46e503450e15dea2b4930b21fc282e2f13714709
hans <hans@burley.(none)>
2013-02-11 18:12:56 -0600

    Finish sign in, few days late due to reading incomprehension

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 SampleApp::Application.routes.draw do
   resources :users
+    resources :sessions, only: [:new, :create, :destroy]
 
-  root to: 'page#home'
+  root to: "static_pages#home"
 
-  match '/echo',    to: 'echo#echo'
+  get "users/new"
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
 
 
 
