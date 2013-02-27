55170e0245fbeb11349937f511e1badb55e4846f
hans <hans@burley.(none)>
2013-02-27 11:24:17 -0600

    Removed files that shouldn't be there in the first place

--- app/controllers/echo_controller.rb
+++
@@ -1,9 +0,0 @@
-class EchoController < ApplicationController
-	def echo
-		puts "++++++++++++++++++++"
-		params.each do |key, value|
-			puts "Param #{key}: #{value}"
-		end
-		puts "++++++++++++++++++++"	
-	end
-end
